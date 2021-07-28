9ef996625050ecb7ff4db987478b8daec23aa760
Songa0 <dolphin2440@naver.com>
2021-07-28 21:42:28 +0900

    Fix popup form design

--- src/components/diary__add/diary_add.jsx
+++ src/components/diary__add/diary_add.jsx
@@ -7,6 +7,11 @@ import {
   faHashtag,
   faTimes,
   faCamera,
+  faEye,
+  faAssistiveListeningSystems,
+  faHandSpock,
+  faTeethOpen,
+  faAirFreshener,
 } from "@fortawesome/free-solid-svg-icons";
 import styles from "./diary_add.module.css";
 
@@ -17,6 +22,11 @@ const DiaryAdd = ({ closePopup, addDiary, fileUploader }) => {
   const endDtRef = useRef();
   const keywordRef = useRef();
   const fileRef = useRef();
+  const sightRef = useRef();
+  const smellRef = useRef();
+  const hearingRef = useRef();
+  const tasteRef = useRef();
+  const touchRef = useRef();
 
   const addDiaryData = async (event) => {
     event.preventDefault();
@@ -50,50 +60,89 @@ const DiaryAdd = ({ closePopup, addDiary, fileUploader }) => {
         />
       </div>
       <form className={styles.form}>
-        <div className={styles.title}>
-          <FontAwesomeIcon icon={faStar} className={styles.icon} />
-          Title
+        <div className={styles.item}>
+          <div className={styles.left}>
+            <div className={styles.title}>
+              <FontAwesomeIcon icon={faStar} className={styles.icon} />
+              Title
+            </div>
+            <input type="text" className={styles.input} ref={titleRef} />
+            <div>
+              <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
+              Location
+            </div>
+            <input type="text" className={styles.input} ref={locationRef} />
+            <div>
+              <FontAwesomeIcon icon={faCalendar} className={styles.icon} />
+              Date
+            </div>
+            <div className={styles.dates}>
+              <input
+                type="date"
+                className={[styles.input, styles.date].join(" ")}
+                placeholder="YYYY-MM-DD"
+                ref={startDtRef}
+              />
+              &nbsp;&nbsp; ~ &nbsp;&nbsp;
+              <input
+                type="date"
+                className={[styles.input, styles.date].join(" ")}
+                placeholder="YYYY-MM-DD"
+                ref={endDtRef}
+              />
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faHashtag} className={styles.icon} />
+              Keyword
+            </div>
+            <input
+              type="text"
+              className={styles.input}
+              ref={keywordRef}
+              placeholder="드라이브, 바다"
+            />
+            <div>
+              <FontAwesomeIcon icon={faCamera} className={styles.icon} />
+              Main Photo
+            </div>
+            <input type="file" className={styles.input} ref={fileRef} />
+          </div>
+          <hr />
+          <div className={styles.right}>
+            <div>
+              <FontAwesomeIcon icon={faEye} className={styles.icon} />
+              Sight
+            </div>
+            <input type="text" className={styles.input} ref={sightRef} />
+
+            <div>
+              <FontAwesomeIcon icon={faAirFreshener} className={styles.icon} />
+              Smell
+            </div>
+            <input type="text" className={styles.input} ref={smellRef} />
+
+            <div>
+              <FontAwesomeIcon icon={faTeethOpen} className={styles.icon} />
+              Taste
+            </div>
+            <input type="text" className={styles.input} ref={tasteRef} />
+
+            <div>
+              <FontAwesomeIcon
+                icon={faAssistiveListeningSystems}
+                className={styles.icon}
+              />
+              Hearing
+            </div>
+            <input type="text" className={styles.input} ref={hearingRef} />
+
+            <div>
+              <FontAwesomeIcon icon={faHandSpock} className={styles.icon} />
+              Touch
+            </div>
+            <input type="text" className={styles.input} ref={touchRef} />
+          </div>
         </div>
-        <input type="text" className={styles.input} ref={titleRef} />
-        <div>
-          <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
-          Location
-        </div>
-        <input type="text" className={styles.input} ref={locationRef} />
-        <div>
-          <FontAwesomeIcon icon={faCalendar} className={styles.icon} />
-          Date
-        </div>
-        <div className={styles.dates}>
-          <input
-            type="date"
-            className={[styles.input, styles.date].join(" ")}
-            placeholder="YYYY-MM-DD"
-            ref={startDtRef}
-          />
-          &nbsp;&nbsp; ~ &nbsp;&nbsp;
-          <input
-            type="date"
-            className={[styles.input, styles.date].join(" ")}
-            placeholder="YYYY-MM-DD"
-            ref={endDtRef}
-          />
-        </div>
-        <div>
-          <FontAwesomeIcon icon={faHashtag} className={styles.icon} />
-          Keyword
-        </div>
-        <input
-          type="text"
-          className={styles.input}
-          ref={keywordRef}
-          placeholder="드라이브, 바다"
-        />
-        <div>
-          <FontAwesomeIcon icon={faCamera} className={styles.icon} />
-          Main Photo
-        </div>
-        <input type="file" className={styles.input} ref={fileRef} />
         <div className={styles.button}>
           <button className={styles.submit} onClick={addDiaryData}>
             Submit
