db027b5643da85dc64bb9d6677dcad8c0d2cbccc
Songa0 <dolphin2440@naver.com>
2021-08-03 21:09:05 +0900

    Add close btn and func

--- src/components/travel/travel.jsx
+++ src/components/travel/travel.jsx
@@ -7,74 +7,68 @@ import {
   faMapMarkerAlt,
   faCalendar,
   faStar,
+  faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import TravelListItem from "../travel__list__item/travel_list_item";
 import Header from "../header/header";
 
-const Travel = ({ authService, dbService }) => {
-  const history = useHistory();
-  const [userId, setUserId] = useState(null);
+const Travel = ({ authService, dbService, openPopup, detailId, userId }) => {
   const [travelDtl, setTravelDtl] = useState({});
   useEffect(() => {
     //history.location.state.detailId로 데이터 가져오기
-    dbService.readDetailData(
-      userId,
-      history.location.state.detailId,
-      (data) => {
-        setTravelDtl(data);
-      }
-    );
-  }, [userId, history.location.state.detailId, dbService]);
-
-  useEffect(() => {
-    authService.onAuthStateChanged((user) => {
-      if (user) {
-        setUserId(user.uid);
-      } else {
-        history.push({
-          pathname: "/",
-        });
-      }
+    dbService.readDetailData(userId, detailId, (data) => {
+      setTravelDtl(data);
+      console.log(data);
     });
-  });
+  }, [dbService, detailId, userId]);
 
-  const goToDayDetail = (event) => {
-    history.push({
-      pathname: `/diary/detail/${event.currentTarget.id}`,
-      state: { day: event.currentTarget.id },
-    });
-  };
-
+  // const goToDayDetail = (event) => {
+  //   history.push({
+  //     pathname: `/diary/detail/${event.currentTarget.id}`,
+  //     state: { day: event.currentTarget.id },
+  //   });
+  // };
+  const closeWindow = () => openPopup(false);
   return (
-    <section className={styles.travel}>
-      <div className={styles.textArea}>
-        <div className={styles.title}>
-          <FontAwesomeIcon icon={faStar} className={styles.title__icon} />
-          <span>{travelDtl.title}</span>
+    <>
+      <header>
+        <FontAwesomeIcon
+          icon={faTimes}
+          size="lg"
+          className={styles.close__icon}
+          onClick={closeWindow}
+        />
+      </header>
+      <section className={styles.travel}>
+        <div className={styles.textArea}>
+          <div className={styles.title}>
+            <FontAwesomeIcon icon={faStar} className={styles.title__icon} />
+            <span>{travelDtl.title}</span>
+          </div>
+          <div className={styles.schedule}>
+            <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
+            <span className={styles.text}> {travelDtl.location}</span>
+          </div>
+          <div className={styles.schedule}>
+            <FontAwesomeIcon icon={faCalendar} className={styles.icon} />
+            <span className={styles.text}>
+              {travelDtl.startDate} ~ {travelDtl.endDate}
+            </span>
+          </div>
+          <ul className={styles.list}>
+            {travelDtl.travel &&
+              Object.keys(travelDtl.travel).map((key) => (
+                <li key={key} id={key}>
+                  <TravelListItem day={key} date={travelDtl.travel[key].date} />
+                </li>
+              ))}
+          </ul>
         </div>
-        <div className={styles.schedule}>
-          <FontAwesomeIcon icon={faMapMarkerAlt} className={styles.icon} />
-          <span className={styles.text}> {travelDtl.location}</span>
+        <div className={styles.map}>
+          <img src="/images/map.png" alt="google map api" />
         </div>
-        <div className={styles.schedule}>
-          <FontAwesomeIcon icon={faCalendar} className={styles.icon} />
-          <span className={styles.text}>
-            {travelDtl.startDate} ~ {travelDtl.endDate}
-          </span>
-        </div>
-        <ul className={styles.list}>
-          {travelDtl.travel &&
-            Object.keys(travelDtl.travel).map((key) => (
-              <li key={key} id={key} onClick={goToDayDetail}>
-                <TravelListItem day={key} date={travelDtl.travel[key].date} />
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div className={styles.map}>
-        <img src="/images/map.png" alt="google map api" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
